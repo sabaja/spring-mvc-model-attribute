2c0aa64233d6b480d12fff8958a006f9528d7dee
sabaja <jacoposabatini76@gmail.com>
2019-03-17 11:43:44 +0100

    Add project

---
+++ src/main/java/com/mvc/modelattribute/ModelAttributeApplication.java
@@ -0,0 +1,13 @@
+package com.mvc.modelattribute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelAttributeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelAttributeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mvc/modelattribute/ModelAttributeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mvc.modelattribute;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ModelAttributeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
