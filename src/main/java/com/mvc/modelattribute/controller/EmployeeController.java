fc32f18c2f4bd1f419f13721f6d569662093cce6
sabaja <jacoposabatini76@gmail.com>
2019-04-13 15:11:25 +0200

    ok

--- src/main/java/com/mvc/modelattribute/controller/EmployeeController.java
+++ src/main/java/com/mvc/modelattribute/controller/EmployeeController.java
@@ -15,7 +15,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.mvc.modelattribute.domain.Employee;
 
 @Controller
-@ControllerAdvice
 @RequestMapping("/spring-mvc-java")
 public class EmployeeController {
 
